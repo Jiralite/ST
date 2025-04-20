eb87e2f5e1fa9eea5900403a6f5b46fd354258c7
Jiralite <33201955+Jiralite@users.noreply.github.com>
2025-04-20 19:32:58 +0100

    fix: Check the illuminati

--- source/utility/configuration.ts
+++ source/utility/configuration.ts
@@ -21,6 +21,7 @@ export const DISCORD_TOKEN = process.env.DISCORD_TOKEN;
 export const ILLUMINATI_GUILD_ID = process.env.ILLUMINATI_GUILD_ID;
 
 export const GUILDS_CHECKING = [
+	ILLUMINATI_GUILD_ID,
 	process.env.GUILD_1,
 	process.env.GUILD_2,
 	process.env.GUILD_3,
